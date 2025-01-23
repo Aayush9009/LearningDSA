afbb632cca2089775c485a6470df92eb66035b76
Aayush Gupta <aayushgupta7321@gmail.com>
2025-01-23 19:27:30 +0530

    LinkedListL2

--- LinkedList/L1_1DLL.cpp
+++ LinkedList/L1_1DLL.cpp
@@ -18,6 +18,7 @@ class Node{
     }
 };
 
+//Code to convert array to LL
 Node* convert2LL(int arr[]){
     Node* head = new Node(arr[0]);
     Node* mover = head;
@@ -28,6 +29,8 @@ Node* convert2LL(int arr[]){
     }
     return head;
 }
+
+//Code to find Length of the LinkedList
 int lengthofLL(Node* head){
     int cnt = 0;
     Node* temp = head;
@@ -37,20 +40,100 @@ int lengthofLL(Node* head){
     }
     return cnt;
 }
+//Code to delete head of the Linkedlist
+Node* removeHead(Node* head){
+    if(head == NULL){
+        return head;
+    }
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+
+//Code to remove the tail of the LL
+Node* removeTail(Node* head){
+    if(head==NULL || head->next == nullptr){
+        return head;
+    }
+    Node* temp = head;
+    while(temp->next->next!=nullptr){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+
+    return head;
+}
+
+//Delete value from the LinkedList
+Node* removeValue(Node* head,int value){
+    if(head==NULL) return head;
+
+    if(head->data==value){
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    Node* temp = head;
+    Node* prev = NULL;
+
+    while(temp!=NULL){
+        if(temp->data==value){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
 int main(){
     int arr[4] = {12, 3, 4, 5};
     Node* y = new Node(arr[0],nullptr);
+    cout<<endl;
     cout<<y->data<<endl;
+
     Node* head = convert2LL(arr);
     Node* temp = head;
 
     cout<<head->data<<" "<<head->next<<" ";
 
+    cout<<endl;
+    //Traversal in a LL
     while(temp){
         cout<<temp->data<<" ";
         temp = temp->next;
     }
 
     cout<<"\nLength of LL is "<<lengthofLL(head)<<endl;
+    
+    // Node* headnew = removeHead(head);
+    // cout<<"New Head after deleting the previous head is "<<headnew->data<<endl;
+
+    cout<<"New LinkedList after deleting the tail is ";
+    head = removeTail(head);
+    temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
+    int value;
+    cout<<"\nEnter the value to be removed-> ";
+    cin>>value;
+
+    Node* head2 = removeValue(head,value);
+    cout<<"\nLinkedList is->  ";
+    Node* temp2 = head2;
+    while(temp2){
+        cout<<temp2->data<<" ";
+        temp2 = temp2->next;
+    }
+
     return 0;
 }
---
+++ LinkedList/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+Node* headnew = removeHead(head);
+    // cout<<"New Head after deleting the previous head is "<<headnew->data<<endl;
